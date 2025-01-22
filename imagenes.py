5018ed4376a8afa18318d623162177a91c2dfe41
yohovani vargas <yohovanivargas@gmail.com>
2025-01-21 19:53:34 -0600

    Primer test in python

--- imagenes.py
+++ imagenes.py
@@ -1,14 +1,25 @@
 from PIL import Image, ImageShow
 
+
 img = Image.open("C:/Users/yohov/OneDrive/Desktop/pokemon/BWP_ES_BW59.png")
 img = img.convert("RGBA")
 
+art = Image.open("C:/Users/yohov/OneDrive/Desktop/pokemon/test.jpg")
 pixdata = img.load()
 
-#C:/Users/yohov/OneDrive/Desktop/pokemon/BWP_ES_BW59.png
+base_width = 204
+
+wpercent = (base_width / float(art.size[0]))
+hsize = int((float(art.size[1]) * float(wpercent)))
+art = art.resize((base_width, hsize), Image.Resampling.LANCZOS)
+
+art = art.convert("RGBA")
+art = art.rotate(90)
+pixtest = art.load()
+
 
 
 for y in range(35,170):
     for x in range(20,224):
-        pixdata[x, y] = (0, 0, 0, 255)
+        pixdata[x, y] = pixtest[x-20, y]
 img.show()
